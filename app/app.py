99530afa6598c92bd87146e5a3e5232bf6e46bf1
Till Meyer zu Westram <till.meyerzuwestram@zuehlke.com>
2024-11-06 14:41:32 +0100

    proxy version from another env var

--- app/app.py
+++ app/app.py
@@ -14,9 +14,16 @@ def env():
     return render_template("env.html", **_template_args(env=dict(os.environ)))
 
 
+def _get_version() -> str:
+    proxy_key = os.environ.get("VERSION_PROXY")
+    if proxy_key:
+        return os.environ.get(proxy_key, "n/a")
+    else:
+        return os.environ.get("VERSION", "n/a")
+
 def _template_args(**extra_args) -> dict:
     default_args = {
-        "version": os.environ.get("VERSION", "n/a"),
+        "version": _get_version(),
         "stage": os.environ.get("STAGE", "dev")
     }
     return {
